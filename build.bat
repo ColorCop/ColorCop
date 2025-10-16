d933d4eb7b9d13dbb8a5930abd538bbd3e02d524
Jay Prall <36337+j4y@users.noreply.github.com>
2025-10-15 21:30:07 -0400

    fix: isWebsafeColor so that it's correct
    Each one has to be a multiple of 51

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-rem vcvars32.bat
-
-rmdir /S /Q Release
-nmake /NOLOGO /f "ColorCop.mak" CFG="ColorCop - Win32 Release"
-
-rmdir /S /Q Debug
-nmake /NOLOGO /f "ColorCop.mak" CFG="ColorCop - Win32 Debug"
