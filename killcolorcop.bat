d5d2227785247aa140df1692843da9596d047f79
Jay Prall <36337+j4y@users.noreply.github.com>
2025-10-18 19:58:08 -0400

    chore: clean up dead batch file

--- killcolorcop.bat
+++
@@ -1,9 +0,0 @@
-echo killcolorcop.bat
-
-del listoutput.txt
-tasklist /FI "IMAGENAME eq colorcop.exe" > listoutput.txt
-
-del foundcolorcop.txt
-findstr ColorCop.ex listoutput.txt > foundcolorcop.txt
-
-rem cmd /c taskkill /F /IM colorcop.exe /T
