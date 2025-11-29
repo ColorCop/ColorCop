54e51a7f086d5ce273766e765d09752e8ee4aeb5
j4y <36337+j4y@users.noreply.github.com>
2025-11-29 16:24:35 -0500

    style: simplify brace placement

--- SystemTray.cpp
+++ SystemTray.cpp
@@ -339,8 +339,7 @@ LRESULT CSystemTray::OnTrayNotification(UINT wParam, LONG lParam)
         pTarget->PostMessage(WM_NULL, 0, 0);
 
         menu.DestroyMenu();
-    }
-    else if (LOWORD(lParam) == WM_LBUTTONDBLCLK)
+    } else if (LOWORD(lParam) == WM_LBUTTONDBLCLK)
     {
 
 
@@ -354,8 +353,7 @@ LRESULT CSystemTray::OnTrayNotification(UINT wParam, LONG lParam)
             if (!menu.LoadMenu(m_tnd.uID)) return 0;
             if (!(pSubMenu = menu.GetSubMenu(0))) return 0;
             uItem = pSubMenu->GetMenuItemID(m_DefaultMenuItemID);
-        }
-        else
+        } else
             uItem = m_DefaultMenuItemID;
 
         pTarget->SendMessage(WM_COMMAND, uItem, 0);
