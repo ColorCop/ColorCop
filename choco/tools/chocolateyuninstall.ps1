71b5b29f4b44cce0b3ff5de5bd28f65265f0b08c
j4y <36337+j4y@users.noreply.github.com>
2026-02-14 18:08:52 -0500

    fix(choco): add explicit installer path to uninstall script to prevent empty exeToRun error

--- choco/tools/chocolateyuninstall.ps1
+++ choco/tools/chocolateyuninstall.ps1
@@ -1,10 +1,13 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'colorcop'
+$toolsDir    = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$installer   = Join-Path $toolsDir 'colorcop-setup.exe'
 
 $packageArgs = @{
     packageName    = $packageName
     fileType       = 'exe'
+    file           = $installer
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     validExitCodes = @(0)
 }
