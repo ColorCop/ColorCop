d9458f9000ee7b346010f45e9134584f65dcfff4
j4y <36337+j4y@users.noreply.github.com>
2026-02-22 22:06:22 -0500

    style: fix inconsistent spacing and formatting

--- SystemTray.cpp
+++ SystemTray.cpp
@@ -92,7 +92,7 @@ BOOL CSystemTray::Create(CWnd* pParent, UINT uCallbackMessage, LPCTSTR szToolTip
                          HICON icon, UINT uID)
 {
     // this is only for Windows 95 (or higher)
-    VERIFY(m_bEnabled = ( GetVersion() & 0xff ) >= 4);
+    VERIFY(m_bEnabled = (GetVersion() & 0xff) >= 4);
     if (!m_bEnabled) return FALSE;
 
     // Make sure Notification window is valid (not needed - CJM)
@@ -153,7 +153,7 @@ void CSystemTray::HideIcon()
 {
     if (m_bEnabled && !m_bHidden) {
         m_tnd.uFlags = NIF_ICON;
-        Shell_NotifyIcon (NIM_DELETE, &m_tnd);
+        Shell_NotifyIcon(NIM_DELETE, &m_tnd);
         m_bHidden = TRUE;
     }
 }
